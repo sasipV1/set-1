d2dd0cdbe196c16dd462123009a2f5c7b468630e
sasipV1 <42263759+sasipV1@users.noreply.github.com>
2018-08-17 11:36:21 +0530

    Update and rename README.md to 1.java

---
+++ 1.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n > 0)
+        {
+            System.out.println("Positive");
+        }
+        else if (n < 0)
+        {
+            System.out.println("Negative");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+	}
+}
